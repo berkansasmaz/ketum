106fe02c3b959b349a2349415fe1bc6b255132bf
berkansasmaz <berkansasmazz@gmail.com>
2019-11-30 22:53:47 +0300

    Added page-head component.

---
+++ Console/src/Ketum.Web/ClientApp/components/shared/page-head.vue
@@ -0,0 +1,24 @@
+<template>
+	<h4 class="font-weight-bold py-3 mb-4">
+		<icon	v-if="icon"	:icon="icon" />	<span class="text-muted font-weight-light">{{ prefix }} / </span> {{ title }}
+	</h4>	
+</template>
+
+<script>
+export default {
+	props: {
+		title: {
+			"default": "",
+			"type" : String
+		},
+		prefix: {
+			"default": "Ketum",
+			"type" : String
+		},
+		icon: {
+			"default": "Ketum",
+			"type" : String
+		}
+	}
+}
+</script>
