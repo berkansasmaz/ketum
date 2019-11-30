639982087c2e13011ad88abe3f9ced246aaeef3a
berkansasmaz <berkansasmazz@gmail.com>
2019-11-30 22:55:49 +0300

    Folder structure changed.

---
+++ Console/src/Ketum.Web/ClientApp/components/root/nav-menu.vue
@@ -0,0 +1,52 @@
+﻿<template>
+
+  <!-- Layout sidenav -->
+  <div
+    id="layout-sidenav"
+    class="sidenav sidenav-vertical layout-sidenav bg-sidenav-theme"
+  >
+    <ul class="sidenav-inner py-1" >
+				<template 	v-for="(route, index) in routes">
+					<li v-if="route.divider" :key="index" class="sidenav-divider mt-0"></li>
+					<li v-else-if="!route.hidden" 
+				class="sidenav-item"
+			
+				:key="index"
+				>
+				<router-link
+				:to="route.path"
+				exact-active-class="active"
+				class="sidenav-link"
+				v-if="route.component"
+				>
+				<icon
+				:icon="route.icon"
+				class="mr-2"
+				/><span>{{ route.display }}</span>
+				</router-link>
+				<a :href="route.path"  v-else class="sidenav-link"> <icon
+				:icon="route.icon"
+				class="mr-2"
+				/><span>{{ route.display }}</span></a>
+				</li>
+				</template>
+    </ul>
+  </div>
+  <!-- / Layout sidenav -->
+</template>
+
+<script>
+import { routes } from "@/router/routes";
+export default {
+  data() {
+    return {
+      routes,
+      collapsed: true
+    };
+  },
+  methods: {}
+};
+</script>
+
+<style scoped>
+</style>
