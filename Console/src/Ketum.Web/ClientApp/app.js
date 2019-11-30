7c684af22f70b2ae2ca614839d8240926a4c8c10
berkansasmaz <berkansasmazz@gmail.com>
2019-11-30 22:57:30 +0300

    Added new components and paths are changed..

--- Console/src/Ketum.Web/ClientApp/app.js
+++ Console/src/Ketum.Web/ClientApp/app.js
@@ -1,26 +1,27 @@
-import Vue from 'vue'
-import axios from 'axios'
-import router from './router/index'
-import store from './store'
-import { sync } from 'vuex-router-sync'
-import App from 'components/app-root'
-import { FontAwesomeIcon } from './icons'
-
+import Vue from 'vue';
+import axios from 'axios';
+import router from './router/index';
+import store from './store';
+import { sync } from 'vuex-router-sync';
+import App from 'components/root/app-root';
+import { FontAwesomeIcon } from './icons';
+import PageHead from 'components/shared/page-head';
 // Registration of global components
-Vue.component('icon', FontAwesomeIcon)
+Vue.component('icon', FontAwesomeIcon);
+Vue.component('page-head', PageHead);
 
-Vue.prototype.$http = axios
+Vue.prototype.$http = axios;
 
-sync(store, router)
+sync(store, router);
 
 const app = new Vue({
-  store,
-  router,
-  ...App
-})
+	store,
+	router,
+	...App
+});
 
 export {
-  app,
-  router,
-  store
-}
+	app,
+	router,
+	store
+};
