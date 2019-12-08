103287f7d75e4eb88c5caa601a13901ab15df153
berkansasmaz <berkansasmazz@gmail.com>
2019-12-08 17:49:07 +0300

    Implemented backend monitoring get api.

--- Console/src/Ketum.Web/ClientApp/services/monitoring.js
+++ Console/src/Ketum.Web/ClientApp/services/monitoring.js
@@ -12,7 +12,16 @@ import {
 		throw result.error;
 	  }
 	},
-  
+	async get(id) {
+		var result = await http.get("/api/v1/monitoring/" + id);
+		if (result.status === 200) {
+		  return result.data;
+		} else {
+		  console.error(result.error);
+		  throw result.error;
+		}
+	  },
+	  
 	async save(value) {
 	  var result = await http.post("/api/v1/monitoring", value);
 	  if (result.status === 200) {
