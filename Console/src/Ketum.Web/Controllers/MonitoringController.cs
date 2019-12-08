fbf878cd64e4ddc86d4d2de93568ed1ae28e9027
berkansasmaz <berkansasmazz@gmail.com>
2019-12-08 17:46:39 +0300

    Added Get for single item and Fixed UserId problem

--- Console/src/Ketum.Web/Controllers/MonitoringController.cs
+++ Console/src/Ketum.Web/Controllers/MonitoringController.cs
@@ -1,33 +1,84 @@
 using System;
 using System.Threading.Tasks;
-using Ketum.Entity;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Ketum.Entity;
+using Ketum.Web;
+using Newtonsoft.Json;
 
-namespace Ketum.Web.Controllers
+namespace Monova.Web.Controllers
 {
     public class MonitoringController : ApiController
     {
-        [HttpGet]
-        public async Task<IActionResult> Get()
+   		 [HttpGet("{id?}")]
+        public async Task<IActionResult> Get([FromRoute]Guid? id)
         {
-			var list = await Db.Monitors.ToListAsync();
+            if (id.HasValue)
+            {
+                if (id.Value == Guid.Empty)
+                {
+                    return Error("You must send monitor id to get.");
+                }
+
+                var monitor = await Db.Monitors.FirstOrDefaultAsync(x => x.MonitorId == id.Value && x.UserId == UserId);
+                if (monitor == null)
+                    return Error("Monitor not found.", code: 404);
+
+                return Success(data: new
+                {
+                    monitor.MonitorId,
+                    monitor.CreatedDate,
+                    monitor.LastCheckDate,
+                    monitor.MonitorStatus,
+                    monitor.Name,
+                    monitor.TestStatus,
+                    monitor.UpTime,
+                    monitor.UpdatedDate
+                });
+            }
+
+            var list = await Db.Monitors.ToListAsync();
             return Success(null, list);
         }
 
         [HttpPost]
-        public async Task<IActionResult> Post([FromBody]KTDMonitor value)
+        public async Task<IActionResult> Post([FromBody]KTMMonitorSave value)
         {
-			if(string.IsNullOrEmpty(value.Name))
-				return Error("Name is required.");
-			value.CreatedDate = DateTime.UtcNow;
-			Db.Monitors.Add(value); 
-			var result = await Db.SaveChangesAsync();
-			if(result > 0)
-            	return Success("Monitoring saved successfully");
-			else
-				return Error("Something is wrong with your model.");
+
+            if (string.IsNullOrEmpty(value.Name))
+            {
+                return Error("Name is required.");
+            }
+            var dataObject = new KTDMonitor
+            {
+				MonitorId = Guid.NewGuid(),
+                CreatedDate = DateTime.UtcNow,
+                Name = value.Name,
+				UserId = UserId
+            };
+			
+			Db.Monitors.Add(dataObject);
+			var monitorStepData = new KTDSMonitorStepSettingsRequest{
+				Url = value.Url
+			};
+
+			var monitorStep =  new KTDMonitorStep{
+				MonitorStepId = Guid.NewGuid(),
+				Type = KTDMonitorStepTypes.Request,
+				MonitorId = dataObject.MonitorId,
+				Settings = JsonConvert.SerializeObject(monitorStepData)//Json' a çevirdik 
+			};
+			Db.MonitorSteps.Add(monitorStep);
+
+            var result = await Db.SaveChangesAsync();
+            if (result > 0)
+                return Success("Monitoring saved successfully.", new
+                {
+                    Id = dataObject.MonitorId
+                });
+            else
+                return Error("Something is wrong with your model.");
         }
     }
 
