bac42f6d9f7a067eac4e9b49da767a1c855a6e22
berkansasmaz <berkansasmazz@gmail.com>
2019-12-08 17:47:50 +0300

    Added new component.

---
+++ Console/src/Ketum.Web/ClientApp/components/Input/text.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="card mb-4">
+      <h6 class="card-header">
+        <div>{{title}}</div>
+      </h6>
+      <div class="card-body">
+        <input 
+			type="text"
+		 	:placeholder="placeholder"
+			class="form-control"
+			v-on:input="$emit('input', $event.target.value)"
+		    ></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+	props: {
+		title : {
+			type: String,
+			default : ""
+		},
+		placeholder: {
+			type: String,
+			default : ""
+		}
+	}
+}
+</script>
