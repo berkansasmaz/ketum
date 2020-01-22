a8859360b92db9ae892136157ad9084e38d74740
berkansasmaz <berkansasmazz@gmail.com>
2020-01-22 17:24:04 +0300

    Model updated for save.

--- Console/src/Ketum.Web/Models/Client/KTMMonitorSave.cs
+++ Console/src/Ketum.Web/Models/Client/KTMMonitorSave.cs
@@ -1,7 +1,10 @@
+using System;
+
 namespace Ketum.Web
 {
     public class KTMMonitorSave
     {
+		public Guid Id { get; set; }
         public string Name { get; set; }
 		public string Url { get; set; }
     }
