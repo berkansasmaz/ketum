5644db4149be54e29214d943171282a402923b02
berkansasmaz <berkansasmazz@gmail.com>
2020-02-06 00:03:03 +0300

    Added new ui component.

---
+++ Console/src/Ketum.Web/ClientApp/components/shared/monitor-status.vue
@@ -0,0 +1,42 @@
+<template>
+	<div>
+		<span :class="'badge badge-pill badge-' + badgeClass">{{this.title}}</span>
+	</div>
+</template>>
+
+<script>
+export default {
+	props:{
+		title: {
+			"default": "Unknown",
+			"type" : String
+		},
+		status: {
+			"default": 0,
+			"type" : Number
+		},
+	},
+	computed: {
+		badgeClass(){
+			switch (this.status) {
+				case 0:
+					return "dark";
+				case 1:
+					return "info";
+				case 2:
+					return "primary";
+				case 3:
+					return "success";
+				case 4:
+					return "warning";
+				case 5:
+					return "danger";
+				case 6:
+					return "dark";
+				default:
+					return "light";
+			}
+		}
+	},
+}
+</script>
