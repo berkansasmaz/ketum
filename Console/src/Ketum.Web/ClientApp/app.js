ff8d5396788ed342149488a8a16caff84bacfa0b
berkansasmaz <berkansasmazz@gmail.com>
2020-02-11 22:52:25 +0300

    Added new components to definition and icons.

--- Console/src/Ketum.Web/ClientApp/app.js
+++ Console/src/Ketum.Web/ClientApp/app.js
@@ -11,11 +11,14 @@ import VueContentPlaceholders from 'vue-content-placeholders';
 import KTIText from 'components/Input/text';
 import VueApexCharts from 'vue-apexcharts';
 import KTVMonitorStatus from 'components/shared/monitor-status';
+import { BootstrapVue} from 'bootstrap-vue';
 
-
+// Install BootstrapVue
+Vue.use(BootstrapVue);
 Vue.use(VueContentPlaceholders);
 
 Vue.use(VueApexCharts);
+Vue.use(require('vue-moment'));
 
 Vue.component('apexchart', VueApexCharts);
 
@@ -29,8 +32,12 @@ Vue.component('icon', FontAwesomeIcon);
 Vue.component('page-head', PageHead);
 Vue.component('ktv-monitor-status', KTVMonitorStatus);
 
-//Registration for Inputs Controls 
+//Registration for Inputs Controls
 Vue.component('mvi-text', KTIText);
+
+// app.js
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.prototype.$http = axios;
 
--- Console/src/Ketum.Web/ClientApp/icons.js
+++ Console/src/Ketum.Web/ClientApp/icons.js
@@ -2,7 +2,22 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 // Official documentation available at: https://github.com/FortAwesome/vue-fontawesome
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-import { faEnvelope, faHeart, faGraduationCap, faHome, faInfo, faList, faSpinner, faPlus, faCreditCard, faChartLine,faUserCircle, faEdit, faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons'
+import {
+  faEnvelope,
+  faHeart,
+  faGraduationCap,
+  faHome,
+  faInfo,
+  faList,
+  faSpinner,
+  faPlus,
+  faCreditCard,
+  faChartLine,
+  faUserCircle,
+  faEdit,
+  faExternalLinkAlt,
+  faSearch
+} from '@fortawesome/free-solid-svg-icons'
 import { faFontAwesome, faMicrosoft, faVuejs } from '@fortawesome/free-brands-svg-icons'
 
 // If not present, it won't be visible within the application. Considering that you
@@ -27,7 +42,8 @@ library.add(
   faCreditCard,
   faChartLine,
   faEdit,
-  faExternalLinkAlt
+  faExternalLinkAlt,
+  faSearch
 )
 
 export {
