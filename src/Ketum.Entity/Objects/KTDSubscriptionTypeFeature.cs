433889ab7af145668fe60007d90e3fc5cc7fd8d5
berkansasmaz <berkansasmazz@gmail.com>
2020-02-28 21:52:29 +0300

    Added sort  to subscription type feature.

--- src/Ketum.Entity/Objects/KTDSubscriptionTypeFeature.cs
+++ src/Ketum.Entity/Objects/KTDSubscriptionTypeFeature.cs
@@ -14,6 +14,7 @@ namespace Ketum.Entity
         public string Description { get; set; }
         public string Name { get; set; }
         public string Value { get; set; }
+        public short Sort { get; set; }
         public bool IsFeature { get; set; }
     }
 }
