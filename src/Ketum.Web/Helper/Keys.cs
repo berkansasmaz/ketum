513452f3bbf5be3c4736a73073d9ed487d94cedc
berkansasmaz <berkansasmazz@gmail.com>
2020-04-06 23:27:50 +0300

    Added config keys.

---
+++ src/Ketum.Web/Helper/Keys.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ketum.Web
+{
+    public static class Keys
+    {
+        public const string ConnectionString = "KETUM_CONNECTIONSTRING";
+        public const string StripeAPIKey = "STRIPE_API_KEY";
+
+    }
+}
