4e5b5630d327b93aa16127088f4fa889b0a6537b
berkansasmaz <berkansasmazz@gmail.com>
2020-10-19 13:09:38 +0300

    Added missing tests.

---
+++ test/Ketum.Application.Tests/Monitors/MonitorAppService_Tests.cs
@@ -0,0 +1,136 @@
+using System;
+using System.Threading.Tasks;
+using NSubstitute;
+using Shouldly;
+using Volo.Abp;
+using Volo.Abp.Domain.Entities;
+using Volo.Abp.Users;
+using Xunit;
+
+namespace Ketum.Monitors
+{
+    public class MonitorAppService_Tests : KetumApplicationTestBase
+    {
+        private readonly IMonitorAppService _monitorAppService;
+        private readonly KetumTestData _testData;
+        private ICurrentUser _currentUser;
+
+
+        public MonitorAppService_Tests()
+        {
+            _currentUser = Substitute.For<ICurrentUser>();
+            _testData = GetRequiredService<KetumTestData>();
+            _monitorAppService = GetRequiredService<IMonitorAppService>();
+        }
+
+        [Fact]
+        public async Task GetListAsync()
+        {
+            Login(_testData.UserId1);
+
+            var monitors = await _monitorAppService.GetListAsync(new GetMonitorsRequestInput());
+
+            monitors.ShouldNotBeNull();
+            monitors.Items.Count.ShouldBeGreaterThan(0);
+        }
+
+        [Fact]
+        public async Task GetAsync()
+        {
+            Login(_testData.UserId1);
+
+            var monitor = await _monitorAppService.GetAsync(_testData.MonitorId1);
+
+            monitor.ShouldNotBeNull();
+            monitor.Id.ShouldBe(_testData.MonitorId1);
+            monitor.Name.ShouldNotBeNullOrWhiteSpace();
+            monitor.Name.ShouldBe(_testData.MonitorName1);
+        }
+
+        [Fact]
+        public async Task GetAsync_Should_Throw_An_Exception_If_Can_Not_Find()
+        {
+            Login(_testData.UserId1);
+
+            var randomMonitorId = Guid.NewGuid();
+
+            await Assert.ThrowsAnyAsync<EntityNotFoundException>(async () =>
+            {
+                await _monitorAppService.GetAsync(randomMonitorId);
+            });
+        }
+
+        [Fact]
+        public async Task CreateAsync()
+        {
+            Login(_testData.UserId1);
+
+             var newMonitor = new CreateMonitorDto
+             {
+                Name = "monitor-name",
+                Url =   "monitor-url",
+                Interval = 1
+             };
+
+             await _monitorAppService.CreateAsync(newMonitor);
+        }
+
+        [Fact]
+        public async Task CreateAsync_Should_Throw_An_Exception_If_Same_Name()
+        {
+            Login(_testData.UserId1);
+
+            var newMonitor = new CreateMonitorDto
+            {
+                Name = _testData.MonitorName1,
+                Url =   "monitor-url",
+                Interval = 1
+            };
+
+            await Assert.ThrowsAnyAsync<BusinessException>(async () =>
+            {
+                await _monitorAppService.CreateAsync(newMonitor);
+            });
+        }
+
+        [Fact]
+        public async Task UpdateAsync()
+        {
+            Login(_testData.UserId1);
+
+            var monitor = new UpdateMonitorDto
+            {
+                Name = "updated-monitor-name",
+                Url =   "updated-monitor-url",
+                Interval = 1
+            };
+
+            await _monitorAppService.UpdateAsync(_testData.MonitorId1, monitor);
+        }
+
+        [Fact]
+        public async Task UpdateAsync_Should_Throw_An_Exception_If_Same_Name()
+        {
+            Login(_testData.UserId1);
+
+            var monitor = new UpdateMonitorDto
+            {
+                Name = _testData.MonitorName2,
+                Url =   "updated-monitor-url",
+                Interval = 1
+            };
+
+            await Assert.ThrowsAnyAsync<BusinessException>(async () =>
+            {
+                await _monitorAppService.UpdateAsync(_testData.MonitorId1, monitor);
+            });
+        }
+
+        
+        private void Login(Guid userId)
+        {
+            _currentUser.Id.Returns(userId);
+            _currentUser.IsAuthenticated.Returns(true);
+        }
+    }
+}
--- test/Ketum.TestBase/KetumTestDataSeedContributor.cs
+++ test/Ketum.TestBase/KetumTestDataSeedContributor.cs
@@ -1,16 +1,81 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using Ketum.Monitors;
+using NSubstitute;
 using Volo.Abp.Data;
 using Volo.Abp.DependencyInjection;
+using Volo.Abp.Guids;
+using Volo.Abp.Users;
 
 namespace Ketum
 {
     public class KetumTestDataSeedContributor : IDataSeedContributor, ITransientDependency
     {
-        public Task SeedAsync(DataSeedContext context)
-        {
-            /* Seed additional test data... */
+        private readonly IMonitorRepository _monitorRepository;
+        private readonly IGuidGenerator _guidGenerator;
+        private readonly KetumTestData _testData;
+        private readonly ICurrentUser _currentUser;
 
-            return Task.CompletedTask;
+        public KetumTestDataSeedContributor(
+            IMonitorRepository monitorRepository, 
+            IGuidGenerator guidGenerator,
+            KetumTestData testData)
+        {
+            _monitorRepository = monitorRepository;
+            _guidGenerator = guidGenerator;
+            _testData = testData;
+            _currentUser = Substitute.For<ICurrentUser>();
+        }
+
+        public async Task SeedAsync(DataSeedContext context)
+        {
+            await BuildMonitorsAsync();
+        }
+
+        private async Task BuildMonitorsAsync()
+        {
+            Login(_testData.UserId1);
+
+            var newMonitorStep = new MonitorStep(
+                _testData.MonitorStepId1,
+                _testData.MonitorId1,
+                _testData.MonitorUrl1,
+                _testData.MonitorInterval1,
+                MonitorStepTypes.Request,
+                MonitorStepStatusTypes.Unknown);
+
+            var monitor = new Monitor(
+                _testData.MonitorId1,
+                _testData.MonitorName1,
+                MonitorStatusTypes.Unknown,
+                newMonitorStep
+            );
+
+            await _monitorRepository.InsertAsync(monitor, true);
+
+            var newMonitorStep2 = new MonitorStep(
+                _testData.MonitorStepId2,
+                _testData.MonitorId2,
+                _testData.MonitorUrl2,
+                _testData.MonitorInterval2,
+                MonitorStepTypes.Request,
+                MonitorStepStatusTypes.Unknown);
+
+            var monitor2 = new Monitor(
+                _testData.MonitorId2,
+                _testData.MonitorName2,
+                MonitorStatusTypes.Unknown,
+                newMonitorStep2
+            );
+
+
+            await _monitorRepository.InsertAsync(monitor2, true);
+        }
+
+        private void Login(Guid userId)
+        {
+            _currentUser.Id.Returns(userId);
+            _currentUser.IsAuthenticated.Returns(true);
         }
     }
 }
